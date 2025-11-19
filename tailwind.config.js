e1419fbd89bbb8c0553c09ac273eed3e6083660c
gamesrjtmp-sys <gamesrjtmp@gmail.com>
2025-11-19 16:57:50 -0500

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // asegura que Tailwind escanee templates y components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
